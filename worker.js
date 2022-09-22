98b7c9d0609724041f7a118e993e03134899203c
Nathan Clevenger <nateclev@gmail.com>
2022-09-22 04:20:10 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -16,7 +16,7 @@ export const api = {
 
 export default {
   fetch: async (req, env) => {
-    const { user, origin, requestId, method, body, time, pathname, pathSegments, pathOptions, query } = await this.env.CTX.fetch(req).then(res => res.json())
+    const { user, origin, requestId, method, body, time, pathname, pathSegments, pathOptions, query } = await env.CTX.fetch(req).then(res => res.json())
     let { delimiter = ',', fields } = pathOptions
     const url = 'https://' + pathOptions ?  pathSegments.slice(1).join('/') : pathSegments.join('/')
     const file = await fetch(url).then(res => res.text())
